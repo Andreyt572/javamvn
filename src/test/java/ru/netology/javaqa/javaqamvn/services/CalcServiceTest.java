cf69b140a83b6f96a331c406b3a7ab5927cb92a5
Andrey Tolokonnikov <andrey572@mail.ru>
2023-05-14 13:28:10 +0300

    [200~

---
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/CalcServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.javaqa.javaqamvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalcServiceTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "10_000, 3_000, 20_000",
+            "100_000, 60_000, 150_000"
+    })
+
+    public void testIsThereEnoughMoney(int income, int expense, int threshold){
+        CalcService service = new CalcService();
+
+        int actual = service.calculate(income, expense, threshold);
+
+        Assertions.assertEquals(income, actual);
+
+
+    }
+}
